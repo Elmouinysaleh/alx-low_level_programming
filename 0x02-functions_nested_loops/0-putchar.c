ac88e21a8035add44bb5bb22705fc6fb220672b4
Elmouiny saleh <elmouinysaleh@gmail.com>
2023-06-20 15:49:34 -0700

    Fixxing error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,8 @@
  * Return: Always 0 (Success)
  */
 
-int main(void){
+int main(void)
+{ 
  _putchar('_');
  _putchar('p');
  _putchar('u');
@@ -15,6 +16,5 @@ int main(void){
  _putchar('a');
  _putchar('r');
  _putchar('\n');
-
  return (0);
 }
