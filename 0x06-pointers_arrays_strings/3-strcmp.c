f533a4cebd4cbc44ca2531e26050d814c840ad38
Elmouiny saleh <elmouinysaleh@gmail.com>
2023-06-29 06:50:00 -0700

    Fixing Strmcp error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,10 +6,15 @@
  */
 
 int _strcmp(char *s1, char *s2) {
-    while (*s1 && (*s1 == *s2)) {
-        s1++;
-        s2++;
-    }
-    
-    return *(unsigned char*)s1 - *(unsigned char*)s2;
+   int i = 0, op = 0;
+
+	while (op == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+
+	return (op);
 }
