14bb1e057acb6dbe9aa6a967d2a2c278fd8ab27d
Elmouiny saleh <elmouinysaleh@gmail.com>
2023-06-29 06:42:58 -0700

    Adding task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *
+ * strcmp - compares two strings
+ *
+ */
+
+int _strcmp(char *s1, char *s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
