1712ada3e7a4ab37adec977bf6c01448690befe4
Elmouiny saleh <elmouinysaleh@gmail.com>
2023-07-13 15:05:36 -0700

    Fixing compile errors

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,20 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "main.h"
 
-// Function to check if a string only contains digits
-int is_digit_string(const char *str) {
+/**
+ * is_digit_string - checks if a string only contains digits
+ * @str: the string to check
+ *
+ * Return: 1 if the string only contains digits, 0 otherwise
+ */
+int is_digit_string(const char *str)
+{
+    if (str == NULL) {
+        return 0;
+    }
     for (int i = 0; str[i] != '\0'; i++) {
         if (!isdigit(str[i])) {
             return 0;
@@ -14,41 +23,66 @@ int is_digit_string(const char *str) {
     return 1;
 }
 
-// Function to multiply two positive numbers
-int multiply(int a, int b) {
+/**
+ * multiply - multiplies two positive numbers
+ * @a: the first number to multiply
+ * @b: the second number to multiply
+ *
+ * Return: the product of the two numbers
+ */
+int multiply(int a, int b)
+{
     return a * b;
 }
 
-int main(int argc, char *argv[]) {
-    // Check the number of arguments
+/**
+ * print_int - prints an integer
+ * @n: the integer to print
+ */
+void print_int(int n)
+{
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+    if (n / 10) {
+        print_int(n / 10);
+    }
+    _putchar(n % 10 + '0');
+}
+
+/**
+ * main - entry point
+ * @argc: the number of arguments
+ * @argv: the arguments array
+ *
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char *argv[])
+{
     if (argc != 3) {
         printf("Error\n");
         return 98;
     }
-    // Allocate memory for the input strings
-    char *num1_str = (char *) malloc(strlen(argv[1]) + 1);
-    char *num2_str = (char *) malloc(strlen(argv[2]) + 1);
+    char *num1_str = malloc(strlen(argv[1]) + 1);
+    char *num2_str = malloc(strlen(argv[2]) + 1);
     if (num1_str == NULL || num2_str == NULL) {
         printf("Error\n");
         return 98;
     }
-    // Copy the input strings to the allocated memory
     strcpy(num1_str, argv[1]);
     strcpy(num2_str, argv[2]);
-    // Check if the input strings are composed of digits
     if (!is_digit_string(num1_str) || !is_digit_string(num2_str)) {
         printf("Error\n");
         free(num1_str);
         free(num2_str);
         return 98;
     }
-    // Convert the input strings to integers
     int num1 = atoi(num1_str);
     int num2 = atoi(num2_str);
-    // Multiply the numbers and print the result
     int result = multiply(num1, num2);
-    printf("%d\n", result);
-    // Free the allocated memory
+    print_int(result);
+    _putchar('\n');
     free(num1_str);
     free(num2_str);
     return 0;
