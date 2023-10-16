5945d526b5f157066bcc4b05f9e2f0585c0f534d
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2023-10-16 12:39:09 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp  https://github.com/Elmouinysaleh/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+wget -p /tmp  https://github.com/Elmouinysaleh/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigalib.so
 export LD_PRELOAD=/tmp/nrandom.so
