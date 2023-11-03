661c53179304747461a15a068554416de7f0bd90
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2023-11-03 14:32:52 +0100

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -6,19 +6,19 @@
 #include <string.h>
 
 /**
- * struct hash_node_s - Node of a hash table
+ * struct shash_node_s - Node of a hash table
  *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
-typedef struct hash_node_s
+typedef struct shash_node_s
 {
 	char *key;
 	char *value;
 	struct hash_node_s *next;
-} hash_node_t;
+} shash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
